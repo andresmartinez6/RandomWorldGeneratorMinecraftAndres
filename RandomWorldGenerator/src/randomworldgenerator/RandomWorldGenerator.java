9d847e895ed441b18a5e57f153149c2c475acc30
andresmartinez6 <andresiz11x@gmail.com>
2023-05-11 17:40:14 +0200

    Minecraft

---
+++ RandomWorldGenerator/src/randomworldgenerator/RandomWorldGenerator.java
@@ -0,0 +1,16 @@
+package randomworldgenerator;
+
+import java.util.Scanner;
+
+
+public class RandomWorldGenerator {
+
+    
+    public static void main(String[] args) {
+        Scanner teclado=new Scanner(System.in);
+        
+        System.out.println("Hola soy Andres y he programado un potente"
+                + "\n mundo de un videojuego de minecraft");
+    }
+    
+}
